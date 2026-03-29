506e01520e44c6db2684f0503bedd7f3946b7c88
Pablo Martín Fernández <pablomrtfdez@gmail.com>
2026-03-29 11:13:20 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -56,7 +56,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'public/PabloMartin_CV', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'MySQL',
